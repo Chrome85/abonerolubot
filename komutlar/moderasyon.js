36c93a0fd94bc6b9cc07e917fe73b8090789e808
Chrome85 <75063366+Chrome85@users.noreply.github.com>
2021-03-04 12:50:12 +0300

    Add files via upload

---
+++ komutlar/moderasyon.js
@@ -0,0 +1,31 @@
+﻿const Discord = require('discord.js');
+
+exports.run = function(client, message) {
+const embed = new Discord.MessageEmbed()
+.setColor('RANDOM')
+.setTitle('» Cowboy bot Moderasyon Komutları')
+.setTimestamp()
+.addField('<a:maviyildiz:793539752858877952> .ban', 'Ban atarsınız')
+.addField('<a:maviyildiz:793539752858877952> .byetkilirol', 'Ban yetkilirol ayarlar')
+.addField('<a:maviyildiz:793539752858877952> .banlog', 'Banlog ayarlar')
+.addField('<a:maviyildiz:793539752858877952> .kick', 'Kick Atarsınız')
+.addField('<a:maviyildiz:793539752858877952> .kicklog', 'Kick Logunu Ayarlarsınız')
+.setImage("https://media.discordapp.net/attachments/769281758977458176/783299061012234260/standard_21.gif")
+.setFooter('Cowboy', client.user.avatarURL())
+.setTimestamp()
+.setThumbnail(client.user.avatarURL())
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'moderasyon',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
